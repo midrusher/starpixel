2e839c54111808080cbbe368bc608655518627ae
Dmitry Ivanov <58045251+midrusher@users.noreply.github.com>
2022-08-22 18:28:53 +0300

    Update info.js

--- src/commands/Misc/info.js
+++ src/commands/Misc/info.js
@@ -10,20 +10,20 @@ module.exports = {
             fetchReply: true,
             ephemeral: true
         });
-        const cmd_name = `ping`
+        const cmd_name = `info`
         const msg = `Основная информация о боте:
 Профиль бота - <@${process.env.bot_id}>
-Разработчик(и) бота - \`<@${process.env.bot_dev.displayName}\`>
+Разработчик(и) бота - <@${process.env.bot_dev.displayName}>
 Описание бота - \`${process.env.bot_descr}\`
 Текущая версия бота - \`v${process.env.bot_v}\`
 Если вы нашли какой-либо баг, опишите его и упомяните разработчика в канале <#849516805529927700>
 
 Ссылки на официальные социальные сети гильдии:
 Discord: \`${process.env.guild_discord}\`
-VK: \`${process.env.guild_vk}\`
-YouTube: \`${process.env.guild_youtube}\`
-TikTok: \`${process.env.guild_tiktok}\`
-Telegram: \`${process.env.guild_telegram}\`
+VK: \`${process.env.guild_vk}\` (не используется)
+YouTube: \`${process.env.guild_youtube}\` (не используется)
+TikTok: \`${process.env.guild_tiktok}\` (не используется)
+Telegram: \`${process.env.guild_telegram}\` (не используется)
 
 Официальная электронная почта гильдии: \`${process.env.guild_email}\``;
 console.log(`${interaction.member.displayName} использовал команду "/${cmd_name}"`)

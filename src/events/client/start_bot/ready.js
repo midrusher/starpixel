baacb650eefd817a38a4183e2c71c48028ecdc40
Dmitry Ivanov <58045251+midrusher@users.noreply.github.com>
2022-09-28 13:18:10 +0300

    Сообщение о запуске бота

--- src/events/client/start_bot/ready.js
+++ src/events/client/start_bot/ready.js
@@ -1,6 +1,6 @@
 const chalk = require(`chalk`);
 const wait = require("timers/promises").setTimeout;
-const { Collection } = require(`discord.js`)
+const { Collection, EmbedBuilder } = require(`discord.js`)
 
 module.exports = {
     name: 'ready',
@@ -15,6 +15,18 @@ module.exports = {
             // Set the key as Guild ID, and create a map which has the invite code, and the number of uses
             await invites.set(guild.id, new Collection(firstInvites.map((invite) => [invite.code, invite.uses])));
             console.log(chalk.blue(`[Бот Starpixel] Приглашения обработаны!`))
+
+            const channel = await guild.channels.fetch(`1016038744312254515`)
+            const timestamp = Math.round(Date.now() / 1000)
+            const embed = new EmbedBuilder()
+            .setTitle(`Бот запущен!`)
+            .setDescription(`Бот ${client.user} был успешно запущен на сервере \`${guild.name}\`! Команды можно использовать! <t:${timestamp}:R>`)
+            .setColor(process.env.bot_color)
+            .setTimestamp(Date.now())
+
+            await channel.send({
+                embeds: [embed]
+            })
         });
     }
 }

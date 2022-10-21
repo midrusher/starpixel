1cdef0a66a90c7f9def911e870f102c41fab8e28
Dmitry Ivanov <58045251+midrusher@users.noreply.github.com>
2022-10-21 13:08:58 +0300

    Бот не включается

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const { Octokit, App } = require(`octokit`)
 const winston = require('winston');
 const chalk = require(`chalk`)
 const { tokenTEST, token, databaseToken, github_token } = process.env;
@@ -8,22 +7,7 @@ const { Client, Collection, GatewayIntentBits, Partials, ActivityType, } = requi
 const fs = require('fs');
 const { DisTube } = require(`distube`);
 const { setInterval } = require('timers/promises');
-const core = require('@actions/core');
-const github = require('@actions/github');
- console.log(`test1`)
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
-console.log(`test2`) 
+
 
 
 

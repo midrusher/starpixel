2e3c03a9406aacb0943a27ab5131320310a5aa7b
Dmitry Ivanov <58045251+midrusher@users.noreply.github.com>
2022-10-25 12:30:32 +0300

    небольшое изменение

---
+++ src/events/client/newUsers/Bot Channel Add.js
@@ -0,0 +1,20 @@
+const chalk = require(`chalk`);
+const { EmbedBuilder } = require("discord.js");
+const { Guild } = require(`../../../schemas/guilddata`)
+
+module.exports = {
+    name: 'guildMemberUpdate',
+    async execute(oldMember, newMember) {
+        const guild_plugin = await oldMember.client.guilds.fetch(`320193302844669959`)
+        const pluginData = await Guild.findOne({ id: guild_plugin.id })
+        if (pluginData.plugins.user_updates === false) return
+        const guild = await oldMember.client.guilds.fetch(`320193302844669959`)
+        const forumChannel = await guild.channels.fetch(`1019649781460639865`)
+        const threadChannel = await forumChannel.threads.fetch(`1032002235938381834`)
+        if (!oldMember.roles.cache.has(`504887113649750016`) && newMember.roles.cache.has(`504887113649750016`)) {
+            await threadChannel.members.add(newMember.user.id, `Вступил в гильдию`)
+        } else if (oldMember.roles.cache.has(`504887113649750016`) && !newMember.roles.cache.has(`504887113649750016`)) {
+            await threadChannel.members.remove(newMember.user.id, `Покинул гильдию`)
+        }
+    }
+}

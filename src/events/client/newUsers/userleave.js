8e2cf23a3c39f79d6a497d254d4d61f457a2c06e
Dmitry Ivanov <58045251+midrusher@users.noreply.github.com>
2022-10-27 18:57:25 +0300

    Небольшое дополнение

--- src/events/client/newUsers/userleave.js
+++ src/events/client/newUsers/userleave.js
@@ -17,7 +17,8 @@ module.exports = {
         const bd = await Birthday.findOne({ userid: member.user.id, guildid: member.guild.id })
         const temp = await Temp.find({ userid: member.user.id, guildid: member.guild.id })
         const tickets = await TicketsUser.find({ userid: member.user.id, guildid: member.guild.id })
-
+        const guild = member.guild
+        const channel = await guild.channels.cache.get(`849608079691350078`)
         if (tickets) {
             tickets.forEach(async (ticket) => {
                 const channel = await member.guild.channels.fetch(ticket.channelid)
@@ -30,18 +31,22 @@ module.exports = {
                 item.delete()
             })
         }
-
+        let info
         if (userData) {
+            info = `Minecraft Nick: \`${userData.nickname}\`
+Minecraft UUID: ${userData.uuid}
+Discord ID: ${userData.userid}
+Реальное имя: ${userData.displayname.name}`
             userData.delete()
         }
         if (bd) {
             bd.delete()
         }
 
-        const guild = member.guild
-        const channel = guild.channels.cache.get(`849608079691350078`)
+
         await channel.send({
-            content: `❌ ${member.user.username} покинул сервер!`
+            content: `❌ ${member.user.username} покинул сервер!
+**Дополнительная информация**: ${info ? info : `Не являлся участником гильдии!`}`
         })
     }
 }
